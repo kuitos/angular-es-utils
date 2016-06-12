1af4fe62afb7129b75841a8c5932c624d12d7b6f
Kuitos <kuitos.lau@gmail.com>
2016-06-12 11:55:45 +0800

    additional type auth

--- src/type-auth/index.js
+++ src/type-auth/index.js
@@ -4,10 +4,6 @@
  * @since 2016-06-07
  */
 
-import angualr from 'angular';
-
-export const isNumber = angualr.isNumber;
-
 const msie = window.document.documentMode;
 export function isClass(func) {
 	// IE 9-11 do not support classes and IE9 leaks with the code below.
@@ -17,4 +13,20 @@ export function isClass(func) {
 	// Workaround for MS Edge.
 	// Check https://connect.microsoft.com/IE/Feedback/Details/2211653
 	return typeof func === 'function' && /^(?:class\s|constructor\()/.test(Function.prototype.toString.call(func));
+}
+
+export function isNumber() {
+	return typeof value === 'number';
+}
+
+export function isRegExp(value) {
+	return Object.prototype.toString.call(value) === '[object RegExp]';
+}
+
+export function isObject(value) {
+	return value !== null && typeof value === 'object';
+}
+
+export function isString(value) {
+	return typeof value === 'string';
 }
